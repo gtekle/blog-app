9b0b45645b6d9b070c5bc6638a778da3589e51ad
tYohannes <teklegy@gmail.com>
2022-05-01 20:56:44 +0300

    Add #most_recent_posts method to User model that returns 3 most recent posts give author id

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def self.most_recent_posts
-    all
+  def self.most_recent_posts(id)
+    Post.order(created_at: :desc).where(author_id: id).first(3)
   end
 end
