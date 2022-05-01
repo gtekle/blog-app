c0138dce8c230dda9dd7a3bc6cb9926845af5271
tYohannes <teklegy@gmail.com>
2022-05-01 21:23:49 +0300

    Add #update_user_posts_counter and #most_recent_comments methods to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def self.update_user_posts_counter(id)
+    user = User.find(id)
+    user.update(posts_counter: 1)
+  end
+
+  def self.most_recent_comments(id)
+    Comment.order(created_at: :desc).where(post_id: id).first(5)
+  end
 end
