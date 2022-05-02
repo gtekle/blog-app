fd5c6072ff28b2c1b91e34c2a4e56c03173e0a27
tYohannes <teklegy@gmail.com>
2022-05-02 12:26:32 +0300

    Refactor #update_post_comments_counter method and call it using after_save Active Record callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def self.update_post_comments_counter(post_id)
+  after_save :update_post_comments_counter
+
+  def update_post_comments_counter
     post = Post.find(post_id)
-    post.update(comments_counter: 1)
+    post.update(comments_counter: post.comments_counter + 1)
   end
 end
