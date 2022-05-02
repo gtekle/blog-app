b8b5211dce0de6c58111a48f791709b3bb6686e8
tYohannes <teklegy@gmail.com>
2022-05-02 12:27:01 +0300

    Refactor #update_post_likes_counter method and call it using after_save Active Record callback

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,10 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def self.update_post_likes_counter(post_id)
+  after_save :update_post_likes_counter
+
+  def update_post_likes_counter
     post = Post.find(post_id)
-    post.update(likes_counter: 1)
+    post.update(likes_counter: post.likes_counter + 1)
   end
 end
