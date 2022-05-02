eb4171b1030d359b843aea0039bdfcb247ebdbfa
tYohannes <teklegy@gmail.com>
2022-05-02 12:25:38 +0300

    Refactor #update_user_posts_counter method and call it using after_save Active Record callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def self.update_user_posts_counter(id)
-    user = User.find(id)
-    user.update(posts_counter: 1)
+  after_save :update_user_posts_counter
+
+  def update_user_posts_counter
+    user = User.find(author_id)
+    user.update(posts_counter: user.posts_counter + 1)
   end
 
-  def self.most_recent_comments(id)
+  def most_recent_comments
     Comment.order(created_at: :desc).where(post_id: id).first(5)
   end
 end
