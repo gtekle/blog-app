c40ce43086833a5630c03c04d2e8f06d30604396
tYohannes <teklegy@gmail.com>
2022-05-03 22:46:16 +0300

    Refactor routes to use nested resources syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 
   # Defines the root path route ("/")
   root "users#index"
