d74e869170a33f6c88f0df5b29ea08983fd07983
tYohannes <teklegy@gmail.com>
2022-05-05 18:39:47 +0300

    Refactored flash-message code to a partial view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,8 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.create(author: current_user, post: @post)
-    redirect_to user_post_path(User.find(params[:user_id]), @post)
+    respond_to do |format|
+      format.html { redirect_to user_post_path(User.find(params[:user_id]), @post), notice: 'You have liked the post!' } if @post
+    end
   end
 end
